49a41dcc89efdcb2ceee4b3986a32940c45534c7
bgveenstra <bgveenstra@users.noreply.github.com>
2015-05-05 14:09:00 -0700

    Update starship.js

--- starship.js
+++ starship.js
@@ -14,12 +14,13 @@ Starship.prototype.getTopSpeed = function () {
 };
 
 Starship.prototype.accelerateTo = function (newSpeed) {
-    if (newSpeed >= 0 && newSpeed <= topSpeed) {
+    if (newSpeed >= 0 && newSpeed <= this.topSpeed) {
         this.currentSpeed = newSpeed;
         return this.currentSpeed;
     }
-    else if (newSpeed > topSpeed) {
+    else if (newSpeed > this.topSpeed) {
         console.log("The starship doesn't go that fast!  Setting speed to top speed.");
+        this.currentSpeed = this.topSpeed;
         return this.topSpeed;
     } else {
         console.log("Negative speed invalid!  No change to speed.");
